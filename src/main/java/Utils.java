a28eecfb1aff69137c0c4bef9db0aae56c109ec7
切克闹大忽悠 <buhaoran2012@163.com>
2023-06-28 12:22:42 +0800

    v2.0

--- src/main/java/Utils.java
+++ src/main/java/Utils.java
@@ -1,8 +1,16 @@
+import java.util.HashMap;
+
 public class Utils {
     public static int SERVER_PORT = 53;
     public static String  LOCAL_DNS_ADDRESS = "202.106.0.20";
+    public static HashMap<String,HashMap<String,String>> cacheMap;
 
-    public static String searchIPFromCache(String DN){
-        return "";
+    static {
+        cacheMap = new HashMap<>();
+    }
+
+    public static HashMap<String,String> searchIPFromCache(String DNS){
+
+        return new HashMap<>();
     }
 }
